d52e0e8efd9809faf603af5a2878635807166392
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-05-23 21:56:43 +0800

    Fix Palette Route with redirect too

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Palette from './Palette';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import seedColors from './seedColors';
 import React, { Component } from 'react';
 import generatePalette from './colorHelpers';
 
 export default class App extends Component {
+	findPalette(id) {
+		return seedColors.find(palette => {
+			return palette.id === id;
+		});
+	}
+
 	render() {
 		return (
 			<Switch>
@@ -12,8 +18,15 @@ export default class App extends Component {
 				<Route
 					exact
 					path="/palette/:id"
-					render={() => <h1>INDIVIDUAL PALETTE</h1>}
+					render={routeProps => (
+						<Palette
+							palette={generatePalette(
+								this.findPalette(routeProps.match.params.id)
+							)}
+						/>
+					)}
 				/>
+				<Redirect path="/" />
 			</Switch>
 			// {/* <div>
 			// 	<Palette palette={generatePalette(seedColors[4])} />
