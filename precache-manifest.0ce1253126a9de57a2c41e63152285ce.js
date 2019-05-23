3224f5af2382f477a40752040da61264458f157c
Jonathan <hj.punzalan@hotmail.com>
2019-05-23 11:24:44 +0800

    Updates

---
+++ precache-manifest.0ce1253126a9de57a2c41e63152285ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e639cf8139cca237848282969acf0cc1",
+    "url": "/colour-project/index.html"
+  },
+  {
+    "revision": "37251dfb449847048aec",
+    "url": "/colour-project/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "9c73d77ec46df8a06aae",
+    "url": "/colour-project/static/css/main.a6cf69e2.chunk.css"
+  },
+  {
+    "revision": "37251dfb449847048aec",
+    "url": "/colour-project/static/js/2.4635bde4.chunk.js"
+  },
+  {
+    "revision": "9c73d77ec46df8a06aae",
+    "url": "/colour-project/static/js/main.4a439c80.chunk.js"
+  },
+  {
+    "revision": "f4db81621c7f847d8c16",
+    "url": "/colour-project/static/js/runtime~main.94375959.js"
+  }
+]);
